714f49a2235ae786b8c1eed31495aac1cf452891
joannayoo <joannayoo@mindslab.ai>
2019-10-17 23:55:42 -0400

    :sparkles: Added PDB featurizer to utils

--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ from rdkit import Chem
 from rdkit.Chem import rdmolfiles
 from rdkit.Chem import rdmolops
 import numpy as np
+import mdtraj as md
+import tempfile
+import os
 
 # TODO change atom list to all atoms
 _possible_atom_list =  ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg',
     'Na', 'Ca', 'Fe', 'As', 'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn',
-    'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 
+    'Ag', 'Pd', 'Co', 'Se', 'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni',
     'Cd', 'In', 'Mn', 'Zr', 'Cr', 'Pt','Hg', 'Pb', 'As', 'UNK']
 _possible_numH_list = [0, 1, 2, 3, 4]
 _possible_valence_list = [0, 1, 2, 3, 4, 5, 6]
@@ -33,13 +36,13 @@ def one_hot_encoding_unk(x, set):
     return list(map(lambda s: x == s, set))
 
 
-def encode_atom(atom, bool_id_feat=False, 
+def encode_atom(atom, bool_id_feat=False,
                 explicit_H=False, use_chirality=False):
     """
     From deepchem.feat.graph_features
     """
 
-    # why not one-hot get_formal_charge and get_num_radical_electrons?      
+    # why not one-hot get_formal_charge and get_num_radical_electrons?
     result = \
         one_hot_encoding_unk(
             atom.GetSymbol(), _possible_atom_list) + \
@@ -50,7 +53,7 @@ def encode_atom(atom, bool_id_feat=False,
         one_hot_encoding_unk(
             atom.GetHybridization(), _possible_hybridization_list) + \
         [atom.GetIsAromatic()]
-            
+
     if not explicit_H:
         result = result + one_hot_encoding_unk(
             atom.GetTotalNumHs(), _possible_numH_list)
@@ -62,56 +65,68 @@ def encode_atom(atom, bool_id_feat=False,
         except:
             result = result + [False, False] + \
                 [atom.HasProp('_ChiralityPossible')]
-                
+
     return np.array(result)
 
 
 def build_graph_from_molecule(mol, use_master_atom=False):
     """
-    From deepchem.ConvMolFeaturizer._featurize
-    Input:
-        rdkit.Chem.rdchem.Mol
-        
+    Param:
+        mol - rdkit.Chem.rdchem.Mol
     Output:
         nodes - np.ndarray of shape (num_atoms, num_feat)
-        canon_adj_list - list. index corresponds to the index of node 
+        canon_adj_list - list. index corresponds to the index of node
                          and canon_adj_list[index] corresponds to indices
-                         of the nodes that node i is connected to. 
+                         of the nodes that node i is connected to.
     """
-    idx_nodes = [(atom.GetIdx(), encode_atom(atom)) 
+    if not isinstance(mol, Chem.rdchem.Mol):
+        raise TypeError("'mol' must be rdkit.Chem.rdchem.Mol obj")
+
+    # what are the two lines below doing?
+    # Answer found in deepchem.data.data_loader featurize_smiles_df
+    # TODO (ytz) this is a bandage solution to reorder the atoms so
+    # that they're always in the same canonical order. Presumably this
+    # should be correctly implemented in the future for graph mols.
+    new_order = rdmolfiles.CanonicalRankAtoms(mol)
+    mol = rdmolops.RenumberAtoms(mol, new_order)
+    
+    
+    idx_nodes = [(atom.GetIdx(), encode_atom(atom))
                  for atom in mol.GetAtoms()]
     idx_nodes.sort()
     _, nodes = list(zip(*idx_nodes))
-    
+
     nodes = np.vstack(nodes)
 
     # Master atom is the "average" of all atoms that is connected to all atom
     # Introduced in https://arxiv.org/pdf/1704.01212.pdf
-    if use_master_atom: 
+    if use_master_atom:
         master_atom_features = np.expand_dims(np.mean(nodes, axis=0), axis=0)
         nodes = np.concatenate([nodes, master_atom_features], axis=0)
-        
-    edge_list = [(bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()) 
+
+    edge_list = [(bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
                 for bond in mol.GetBonds()]
-    
-    canon_adj_list = [[] for _ in range(len(nodes))] 
-    
+
+    canon_adj_list = [[] for _ in range(len(nodes))]
+
     for edge in edge_list:
         canon_adj_list[edge[0]].append(edge[1])
         canon_adj_list[edge[1]].append(edge[0])
-        
+
     if use_master_atom:
         fake_atom_index = len(nodes) - 1
-        
+
         for i in range(len(nodes) - 1):
             canon_adj_list[i].append(fake_atom_index)
-        
+
     return (nodes, canon_adj_list)
 
 
 def smiles2graph(smiles_arr):
     """
     from deepchem.data.data_loader.featurize_smiles_df
+    
+    smiles_arr: list of smiles str
     """
     features = []
     invalid_ind = []
@@ -120,14 +135,6 @@ def smiles2graph(smiles_arr):
         try:
             mol = Chem.MolFromSmiles(smiles)
 
-            # what are the two lines below doing?
-            # Answer found in deepchem.data.data_loader featurize_smiles_df
-            # TODO (ytz) this is a bandage solution to reorder the atoms so
-            # that they're always in the same canonical order. Presumably this
-            # should be correctly implemented in the future for graph mols.
-            new_order = rdmolfiles.CanonicalRankAtoms(mol)
-            mol = rdmolops.RenumberAtoms(mol, new_order)
-
             feature = build_graph_from_molecule(mol)
             features.append(feature)
 
@@ -135,3 +142,47 @@ def smiles2graph(smiles_arr):
             invalid_ind.append(ind)
 
     return features, invalid_ind
+
+
+def combine_mdtraj(protein_traj, ligand_traj):
+    chain = protein_traj.topology.add_chain()
+    residue = protein_traj.topology.add_residue("LIG", chain, resSeq=1)
+    for atom in ligand_traj.topology.atoms:
+        protein_traj.topology.add_atom(atom.name, atom.element, residue)
+    protein_traj.xyz = np.hstack([protein_traj.xyz, ligand_traj.xyz])
+    protein_traj.topology.create_standard_bonds()
+
+    return protein_traj
+
+
+def pdb2graph(pdbid, data_dir='./data/pdbbind/v2018'):
+    """
+    pdbid: str. protein code from PDBBind
+    graph_type: str. One of ['protein', 'ligand', ]
+    """
+    
+    protein_pdb_file = os.path.join(
+        data_dir, pdbid, "{}_protein.pdb".format(pdbid))
+    ligand_pdb_file = os.path.join(
+        data_dir, pdbid, "{}_ligand.pdb".format(pdbid))
+
+    if not os.path.exists(protein_pdb_file) or \
+        not os.path.exists(ligand_pdb_file):
+        raise IOError(".pdb file not found in {}".format(
+            os.path.join(data_dir, pdbid)))
+
+    # combining protein pdb file and ligand pdb file to one pdb file   
+    protein_traj = md.load(protein_pdb_file)
+    ligand_traj = md.load(ligand_pdb_file)
+
+    complex_traj = combine_mdtraj(protein_traj, ligand_traj)
+    tempdir = tempfile.mkdtemp()
+    complex_traj.save(os.path.join(tempdir, 'complex.pdb'))
+
+    protein = rdmolfiles.MolFromPDBFile(protein_pdb_file)
+    ligand = rdmolfiles.MolFromPDBFile(ligand_pdb_file)
+    compl = rdmolfiles.MolFromPDBFile(os.path.join(tempdir, 'complex.pdb'))
+        
+    return (build_graph_from_molecule(protein),
+            build_graph_from_molecule(ligand),
+            build_graph_from_molecule(compl))
